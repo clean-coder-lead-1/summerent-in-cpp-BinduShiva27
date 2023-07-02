f0b40e52c7fa704fa382bf074f52fd670cd40af7
BinduShiva27 <105207888+BinduShiva27@users.noreply.github.com>
2023-07-02 19:23:41 +0530

    Lcov Test Updated

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -5,4 +5,19 @@
 
 TEST_CASE("infers the breach according to limits") {
   REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
+  REQUIRE(inferBreach(32, 20, 30) == TOO_HIGH);
+  REQUIRE(inferBreach(25, 20, 30) == NORMAL);
+}
+TEST_CASE("Classifies Temperature Breach according to Cooling Type") {
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING,25) == NORMAL);
+  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING,25) == NORMAL);
+  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING,25) == NORMAL);
+}
+TEST_CASE("Check and Alert") {
+  BatteryCharacter BtrChar;
+  BtrChar.coolingType = PASSIVE_COOLING;
+  checkAndAlert(TO_CONTROLLER, BtrChar, 30);
+  checkAndAlert(TO_EMAIL, BtrChar, 30);
+  checkAndAlert(TO_EMAIL, BtrChar, -10);
+  checkAndAlert(TO_EMAIL, BtrChar, 40);
 }
